4b55a5a70d2a6e4f5ea378954b6036f980962ee7
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2025-10-27 09:59:03 +0100

    primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { io, Socket } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('ProjecteNF1CriptografiaiSocolsRA5iRA3A13');
+export class App implements OnInit {
+  protected readonly title = signal('ProjecteNF1CriptografiaiSocolsRA5iRA3A13')
+  private socket!: Socket;
+  videos: string[] = [];
+  missatge: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.socket = io('http://localhost:3000', {
+      transports: ['websocket'],
+    });
+
+    this.socket.on('connectionPC', (data: string[]) => {
+      console.log('Datos recibidos:', data)
+      this.videos = data
+    });
+  }
 }
