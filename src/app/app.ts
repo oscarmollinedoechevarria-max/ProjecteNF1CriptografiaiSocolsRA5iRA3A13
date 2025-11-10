24d5dafe03b18fb8cb29339b8e62681f75394522
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2025-11-10 09:20:35 +0100

    poder selecionar video

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css'
@@ -14,6 +15,7 @@ export class App implements OnInit {
   protected readonly title = signal('ProjecteNF1CriptografiaiSocolsRA5iRA3A13');
   private socket!: Socket;
   videos: string[] = [];
+  videoSelecionat: string = "";
   videoUrl: string | null = null;
   codigo: string | null = null;
   idVideo!: number;
@@ -33,19 +35,19 @@ export class App implements OnInit {
       this.videos = data;
     });
 
-    this.socket.on('videoAsignado', (data: any) => {
-      this.videoUrl = null;
-      this.codigo = data.codigo;
-      this.idVideo = data.id;
-      this.missatge = 'Codi assignat: ' + data.codigo + '. Introdueix-lo a A2 per obtenir permís.';
-    });
-
-    this.socket.on('permisoVideo', (data: any) => {
-      if (data.id === this.idVideo) {
-        this.videoUrl = 'http://localhost:3000/videos/' + data.nombre_archivo;
-        this.missatge = 'Permís concedit, pots reproduir el vídeo';
-      }
-    });
+    // this.socket.on('videoAsignado', (data: any) => {
+    //   this.videoUrl = null;
+    //   this.codigo = data.codigo;
+    //   this.idVideo = data.id;
+    //   this.missatge = 'Codi assignat: ' + data.codigo + '. Introdueix-lo a A2 per obtenir permís.';
+    // });
+    //
+    // this.socket.on('permisoVideo', (data: any) => {
+    //   if (data.id === this.idVideo) {
+    //     this.videoUrl = 'http://localhost:3000/videos/' + data.nombre_archivo;
+    //     this.missatge = 'Permís concedit, pots reproduir el vídeo';
+    //   }
+    // });
   }
 
   demanarVideo() {
