d850138cabaf7bb972411b9feeb9462b47829064
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2026-02-02 12:13:15 +0100

    implementacio ssr

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,47 +11,47 @@ import {FormsModule} from '@angular/forms';
   standalone: true,
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  protected readonly title = signal('ProjecteNF1CriptografiaiSocolsRA5iRA3A13');
-  private socket!: Socket;
-  videos: string[] = [];
-  videoSelecionat: string = "";
-  videoUrl: string | null = null;
-  codigo: string | null = null;
-  idVideo!: number;
-  missatge: string = '';
-
-
-
-  ngOnInit(): void {
-    this.socket = io('http://localhost:3000', { transports: ['websocket'] });
-
-    this.socket.on('connect', () => {
-      this.socket.emit('registerPlatform', 'PC');
-      this.socket.emit('llistaVideos');
-    });
-
-    this.socket.on('videos', (data: string[]) => {
-      this.videos = data;
-    });
-
-    this.socket.on('videoAsignado', (data: any) => {
-      this.videoUrl = null;
-      this.codigo = data.codigo;
-      this.idVideo = data.id;
-      this.missatge = 'Codi assignat: ' + data.codigo + '. Introdueix-lo a A2 per obtenir permís.';
-    });
-
-    this.socket.on('permisoVideo', (data: any) => {
-      if (data.id === this.idVideo) {
-        this.videoUrl = 'http://localhost:3000/videos/' + data.nombre_archivo;
-        this.missatge = 'Permís concedit, pots reproduir el vídeo';
-      }
-    });
-  }
-
-  demanarVideo() {
-    if (!this.videoSelecionat) return;
-    this.socket.emit('pedirVideo', { nombre_archivo: this.videoSelecionat });
-  }
+export class App /*implements OnInit*/ {
+  // protected readonly title = signal('ProjecteNF1CriptografiaiSocolsRA5iRA3A13');
+  // private socket!: Socket;
+  // videos: string[] = [];
+  // videoSelecionat: string = "";
+  // videoUrl: string | null = null;
+  // codigo: string | null = null;
+  // idVideo!: number;
+  // missatge: string = '';
+  //
+  //
+  //
+  // ngOnInit(): void {
+  //   this.socket = io('http://localhost:3000', { transports: ['websocket'] });
+  //
+  //   this.socket.on('connect', () => {
+  //     this.socket.emit('registerPlatform', 'PC');
+  //     this.socket.emit('llistaVideos');
+  //   });
+  //
+  //   this.socket.on('videos', (data: string[]) => {
+  //     this.videos = data;
+  //   });
+  //
+  //   this.socket.on('videoAsignado', (data: any) => {
+  //     this.videoUrl = null;
+  //     this.codigo = data.codigo;
+  //     this.idVideo = data.id;
+  //     this.missatge = 'Codi assignat: ' + data.codigo + '. Introdueix-lo a A2 per obtenir permís.';
+  //   });
+  //
+  //   this.socket.on('permisoVideo', (data: any) => {
+  //     if (data.id === this.idVideo) {
+  //       this.videoUrl = 'http://localhost:3000/videos/' + data.nombre_archivo;
+  //       this.missatge = 'Permís concedit, pots reproduir el vídeo';
+  //     }
+  //   });
+  // }
+  //
+  // demanarVideo() {
+  //   if (!this.videoSelecionat) return;
+  //   this.socket.emit('pedirVideo', { nombre_archivo: this.videoSelecionat });
+  // }
 }
